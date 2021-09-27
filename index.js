c7bb373a20610369df662923aa5ce55832947072
andrejblesic <blesic.andrej@gmail.com>
2021-09-27 21:50:18 +0200

    grid, zoom, object drawn as red circle

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let scaleFactor = 10000 / interval;
 let counter = 0;
 let tickRate = 250;
 let secondCounter = 0;
+let zoomedIn = false;
 
 const c = document.getElementById("canvas");
 const ctx = c.getContext("2d");
@@ -25,7 +26,7 @@ class Circle {
   }
 }
 
-function drawGrid(x) {
+function drawLine(x) {
   ctx.beginPath();
   ctx.setLineDash([5, 15]);
   ctx.moveTo(x, 0);
@@ -33,14 +34,19 @@ function drawGrid(x) {
   ctx.stroke();
 }
 
-for (let i = 1; i < 15; i++) {
-  drawGrid(i*100);
+
+function drawGrid() {
+  for (let i = 1; i < 16; i++) {
+    drawLine(i*100);
+  }
 }
+
+drawGrid();
 
 let circleRadius = 0;
 let x = 300;
 let objSpeed = 200 / scaleFactor;
-const speedOfSound = 343 / scaleFactor;
+let speedOfSound = 343 / scaleFactor;
 
 document.getElementById('speed-input').addEventListener('input', e => {
   objSpeed = Number(e.target.value) / scaleFactor;
@@ -60,6 +66,33 @@ document.getElementById('stop').addEventListener('click', e => {
   stopSimulation();
 });
 
+document.getElementById('zoom-in').addEventListener('click', e => {
+  reset();
+  e.target.setAttribute('disabled', true);
+  document.getElementById('zoom-out').removeAttribute('disabled');
+  zoomedIn = true;
+  scaleFactor = 1000 / interval;
+  let allSpans = document.getElementsByTagName('SPAN');
+  objSpeed = 200 / scaleFactor;
+  speedOfSound = 343 / scaleFactor;
+  for (span of allSpans) {
+    span.innerHTML = parseInt(span.innerHTML) / 10 + 'm';
+  }
+});
+
+document.getElementById('zoom-out').addEventListener('click', e => {
+  reset();
+  e.target.setAttribute('disabled', true);
+  document.getElementById('zoom-in').removeAttribute('disabled');
+  scaleFactor = 10000 / interval;
+  let allSpans = document.getElementsByTagName('SPAN');
+  objSpeed = 200 / scaleFactor;
+  speedOfSound = 343 / scaleFactor;
+  for (span of allSpans) {
+    span.innerHTML = parseInt(span.innerHTML) * 10 + 'm';
+  }
+});
+
 document.getElementById('reset').addEventListener('click', () => {
   reset();
 });
@@ -69,10 +102,9 @@ let circles = [];
 circles.push(new Circle(x, canvasHeight/2, 0));
 
 function draw() {
+  ctx.strokeStyle = "#000000";
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-  for (let i = 1; i < 16; i++) {
-    drawGrid(i*100);
-  }
+  drawGrid();
   ctx.moveTo(0, canvasHeight/2);
   ctx.setLineDash([5, 5]);
   ctx.lineTo(canvasWidth, canvasHeight/2);
@@ -84,6 +116,12 @@ function draw() {
     ctx.stroke();
     ctx.beginPath();
   });
+  ctx.strokeStyle = "#FF0000";
+  ctx.arc(x, canvasHeight/2, 5, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fillStyle = '#ff0000';
+  ctx.fill();
+  ctx.beginPath();
   x+=objSpeed;
   counter++;
 }
@@ -103,10 +141,9 @@ function startSimulation() {
 
   counterInterval = setInterval(() => {
     secondCounter+=0.05;
-    document.getElementById('seconds').innerHTML = secondCounter;
+    document.getElementById('seconds').innerHTML = secondCounter.toFixed(2);
   }, 50)
 }
-
 
 function stopSimulation() {
   clearInterval(circleInterval);
@@ -115,9 +152,11 @@ function stopSimulation() {
 }
 
 function reset() {
+  ctx.strokeStyle = "#000000";
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
   circles = [];
   x = 300;
   secondCounter = 0;
   counter = 0;
+  drawGrid();
 }
