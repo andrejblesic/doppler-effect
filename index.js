a25a438cf5dc82afc767aec8a51668354ef47026
andrejblesic <blesic.andrej@gmail.com>
2021-09-30 16:56:07 +0200

    minor revisions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight;
 const canvasWidth = Math.floor(windowWidth / 100) * 100 - 300;
-const canvasHeight = Math.floor(windowHeight / 100) * 100 - 100;
+const canvasHeight = Math.floor(windowHeight / 100) * 100 - 50;
 const interval = 48;
 let tickRate = 300;
 let scaleFactor = 10000 / interval;
@@ -17,8 +17,8 @@ let strokeLineWidth = 1;
 let funMode = false;
 
 //elements
-const ruler = document.getElementById('ruler');
 const displaySecondsElapsed = document.getElementById('seconds');
+const ruler = document.getElementById('ruler');
 const stopBtn = document.getElementById('stop');
 const resetBtn =  document.getElementById('reset');
 const zoomInBtn = document.getElementById('zoom-in');
@@ -75,6 +75,19 @@ function drawGrid() {
 
 drawGrid();
 
+// draw the moving object dot onto the canvas
+function drawObject() {
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "#0000FF"; // object color
+  ctx.fillStyle = '#0000FF'; // object color
+  ctx.beginPath();
+  ctx.arc(x, canvasHeight/2, 5, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.strokeStyle = "#000";
+}
+
+drawObject();
+
 // draw legend at bottom of canvas for given canvas width
 (function drawRuler() {
   ruler.innerHTML = '';
@@ -107,6 +120,9 @@ startingPosInput.addEventListener('input', e => {
     startingPos = (Number(e.target.value) / 10) + 100;
   }
   x = startingPos;
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  drawGrid();
+  drawObject();
 });
 
 startBtn.addEventListener('click', e => {
@@ -185,13 +201,7 @@ lineWidth.addEventListener('input', e => {
     ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
     ctx.stroke();
   });
-  ctx.lineWidth = 1;
-  ctx.strokeStyle = "#0000FF"; // object color
-  ctx.fillStyle = '#0000FF'; // object color
-  ctx.beginPath();
-  ctx.arc(x, canvasHeight/2, 5, 0, 2 * Math.PI);
-  // ctx.stroke();
-  ctx.fill();
+  drawObject();
   ctx.strokeStyle = "#000";
 });
 
@@ -226,22 +236,11 @@ function draw() {
     ctx.beginPath();
     ctx.strokeStyle = circle.color;
     ctx.lineWidth = strokeLineWidth;
-    // if (circle.color === 'red') {
-    // } else {
-    //   ctx.lineWidth = 1;
-    // }
     circle.setRadius(circle.radius + speedOfSound);
     ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
     ctx.stroke();
   });
-  ctx.lineWidth = 1;
-  ctx.beginPath();
-  ctx.strokeStyle = "#0000FF"; // object color
-  ctx.fillStyle = '#0000FF'; // object color
-  ctx.arc(x, canvasHeight/2, 5, 0, 2 * Math.PI);
-  ctx.stroke();
-  ctx.fill();
-  // ctx.beginPath();
+  drawObject();
   xCoordDisplay.innerHTML = !zoomedIn ? ((x - scaleFactor/2) * 10).toFixed(2) : ((((x - scaleFactor/2) * 10).toFixed(2) - 900) / 10).toFixed(2);
 }
 
@@ -274,7 +273,7 @@ function startSimulation() {
       0,
       funMode ? colorArray[Math.floor(Math.random() * colorArray.length)] : '#000'
     ));
-    if (tickCounter > 200) {
+    if (tickCounter > 250) {
       circles.shift();
     }
   }, tickRate);
@@ -301,24 +300,20 @@ function stopSimulation() {
 // reset to initial state
 function reset() {
   zoomOutBtn.click();
-  // scaleDisplay.innerHTML = '1';
   zoomedIn = false;
   ctx.strokeStyle = "#000000";
   ctx.fillStyle = '#000000';
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-  // startingPosInput.value = 0;
-  // speedInput.value = 200;
-  // accelerationInput.value = 0;
   displaySecondsElapsed.innerHTML = '0';
   xCoordDisplay.innerHTML = '0';
-  drawGrid();
   circles = [];
   scaleFactor = 10000 / interval;
   tickCounter = 0;
   secondCounter = 0;
-  accelerationRate = 0;
+  // accelerationRate = 0;
   acceleration = 0;
   startingPos = 100;
   x = startingPos;
-  // objSpeed = 200 / scaleFactor;
+  drawGrid();
+  drawObject();
 }
